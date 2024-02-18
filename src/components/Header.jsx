6b0f97f03553a685e58d204ac7d702deff6e502a
Anas Bashir <55910194+an4s911@users.noreply.github.com>
2024-02-18 11:37:33 +0300

    Finished header design mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,15 @@
+import Logo from "./Logo";
+import { HiMenu } from "react-icons/hi";
 
 function Header() {
-    return <header></header>;
+    return <header className="">
+        <div className="flex shadow-lg bg-white justify-between items-center p-3">
+            <div className="logo">
+                <Logo size={7} />
+            </div>
+            <HiMenu size={28} className="cursor-pointer" />
+        </div>
+    </header>;
 }
 
 export default Header;
