c817cdc419c75284b835b91a48bac82aa6f9b4db
Anas Bashir <55910194+an4s911@users.noreply.github.com>
2024-02-18 11:39:34 +0300

    Side Nav Bar transition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,27 @@
 import Logo from "./Logo";
 import { HiMenu } from "react-icons/hi";
+import { CSSTransition } from "react-transition-group";
+import { useState, useRef } from "react";
 
 function Header() {
-    return <header className="">
+    const [sideNavIn, setSideNavIn] = useState(false);
+    const sideNavBar = useRef(null);
+    const sideNavShadow = useRef(null);
+
+    return <header className="relative z-10">
         <div className="flex shadow-lg bg-white justify-between items-center p-3">
             <div className="logo">
                 <Logo size={7} />
             </div>
-            <HiMenu size={28} className="cursor-pointer" />
+            <HiMenu size={28} className="cursor-pointer" onClick={() => setSideNavIn(!sideNavIn)} />
         </div>
+        <CSSTransition nodeRef={sideNavBar} in={sideNavIn} timeout={300} classNames="side-nav" unmountOnExit>
+            <div ref={sideNavBar} className="bg-white absolute -z-20 right-0 w-3/4 h-screen"></div>
+        </CSSTransition>
+        <CSSTransition nodeRef={sideNavShadow} in={sideNavIn} timeout={300} classNames="fade" unmountOnExit>
+            <div ref={sideNavShadow} className="h-screen w-full absolute -z-30 right-0 bg-black opacity-40" onClick={() => setSideNavIn(!sideNavIn)}>
+            </div>
+        </CSSTransition>
     </header>;
 }
 
