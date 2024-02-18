897e8af422c8cd989394cc6be68a4b078b6ca0f7
Anas Bashir <55910194+an4s911@users.noreply.github.com>
2024-02-18 23:14:57 +0300

    Responsive Main component and card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
 function Card({ image, heading, paragraph, buttonText, buttonLinkUrl, bgColor }) {
-    return <div className="grid grid-cols-1 gap-4 odd:bg-secondary even:bg-tertiary" style={{ backgroundColor: bgColor }}>
+    return <div className="card grid grid-cols-1 md:grid-cols-2 md:gap-0 gap-4 odd:bg-secondary even:bg-tertiary items-center" style={{ backgroundColor: bgColor }}>
 
         <div>
-            <img src={image} alt="" />
+            <img src={image} alt="" className="w-full" />
         </div>
         <div className="flex flex-col items-center text-center py-10 px-10 gap-4">
             <h3 className="text-2xl font-bold">{heading}</h3>
